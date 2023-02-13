5c89ade6f52314686d92ca5ad0977bedd292c7c2
DanielFabianAlcon <124622837+DanielFabianAlcon@users.noreply.github.com>
2023-02-13 21:07:03 +0100

    Second

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,118 @@
+
+// Exercise 1   Lesson 10
+const country = 'España';
+const continent = 'Europa';
+let population = 45;
+// Exercise 2   Lesson 10
+console.log(country, continent, population);
+
+
+
+// Exercise 1   Lesson 12
+const isIsland = false;
+let language;
+// Exercise 2   Lesson 12
+console.log(isIsland, population, country, language);
+
+
+
+// Exercise 1   Lesson 13
+language = 'Spanish';
+// Exercise 2   Lesson 13
+//change variables to const
+// Exercise 1   Lesson 13
+//change value of varibles
+
+
+
+// Exercise 1   Lesson 14
+const halfPopulation = population / 2;
+console.log(halfPopulation);
+// Exercise 2   Lesson 14
+console.log(halfPopulation + 1);
+// Exercise 3   Lesson 14
+console.log(halfPopulation > 6);
+// Exercise 4   Lesson 14
+console.log(halfPopulation > 33);
+// Exercise 5   Lesson 14
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description);
+
+
+
+// Exercise 1   Lesson 17
+console.log(`${country} is in ${continent}, and its ${population} million people speak ${language}`);
+
+
+
+// Exercise 1   Lesson 18
+let notEnought;
+if (population > 33) {
+    console.log(`${country}'s population is above average`);
+} else {
+    notEnought = 33 - population;
+    console.log(`${country}'s population is ${notEnought} below average`);
+}
+// Exercise 2   Lesson 18
+// change variables to 13 and 130 for check results
+
+
+
+// Exercise 1   Lesson 20
+//predicted
+// Exercise 2   Lesson 20
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + '17');
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// Exercise 1   Lesson 22
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+// Exercise 2   Lesson 22
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No neighbours');
+}
+// Exercise 3   Lesson 22
+//make elseif on ex2
+// Exercise 4   Lesson 22
+//make else block on ex2
+// Exercise 5   Lesson 22
+//use prompt as 1 or 0
+// Exercise 6   Lesson 22
+// use === not ==
+// Exercise 7   Lesson 22
+//convert to a number
+// Exercise 8   Lesson 22
+//think about it
+
+
+// Exercise 1   Lesson 26
+const languageSpeak = 'arabic';
+switch (languageSpeak) {
+    case 'chinese':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+        break;
+}
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,221 @@
-let js = 'amazing';
+/*let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
-40 - 15 + 2;
 console.log(40 - 15 + 2);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1996;
+//birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Fabian';
+console.log(lastName);
+
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Edison';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+with
+multiple
+lines`);
+
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    let century = 20;
+} else {
+    let century = 21;
+}
+console.log(century);
+
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23));
+
+//type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('I am ' + String(23) + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' / '2');
+
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined')
+} else {
+    console.log('Height is UNDEFINED')
+}
+
+const age = 18;
+if (age === 18) console.log('You just became adult (strict)');
+if (age == 18) console.log('You just became adult (loose)');
+
+const favourite = Number(prompt("What's your favorite number?"));
+console.log(favourite);
+console.log(typeof favourite)
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number!');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number');
+} else if (favourite === 9) {
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 23 or 7 or 9')
+}
+
+if (favourite !== 23) console.log('Why not 23?');
+
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+//if (shouldDrive) {
+//    console.log('Sarah is able to drive');
+//} else {
+//    console.log('Someone else should drive');
+//}
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive');
+} else {
+    console.log('Someone else should drive');
+}
+
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan couse structutre');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+*/
